dc159428cc11aa6341b22a743b54528a929313ee
TrumanBrown <70858679+TrumanBrown@users.noreply.github.com>
2020-12-09 11:28:13 -0600

    working commit - Created using LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -2,7 +2,7 @@ class Solution {
     public int firstUniqChar(String s) {
         int[] letters= new int[26];
         
-        for(int i=0; i<s.length(); i++)
+        for(int i=0; i<s.length(); i++) //abcb will go to {1,2,1,0,0,0...}
         {
             letters[s.charAt(i)-'a']++;
         }
@@ -15,7 +15,28 @@ class Solution {
             }
         }
         
-        
         return -1;
     }
 }
+    //     char[] c= s.toCharArray();
+    //     boolean repeats=false;
+    //     if(c.length==1)
+    //     {
+    //         return 0;
+    //     }
+    //     for(int i=0; i<c.length; i++)
+    //     {
+    //         repeats=false;
+    //         for(int j=0; j<c.length; j++)
+    //         {
+    //             if(c[i]==c[j] && i!=j)
+    //             {
+    //                 repeats=true;
+    //             }
+    //         }
+    //         if(!repeats)
+    //         {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
